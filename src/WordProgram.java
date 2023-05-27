d437a01e3b11c97f0d514f95c4c85b9e26c43a7f
tracyqiao <tracyqiao413@gmail.com>
2023-05-26 20:16:10 -0700

    progress

--- src/WordProgram.java
+++ src/WordProgram.java
@@ -1,144 +1,167 @@
 import javax.swing.*;
+import javax.swing.text.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
+import java.awt.event.*;
 
+public class WordProgram extends JFrame {
+    private JTextPane textPane;
+    private JComboBox<String> fontComboBox;
+    private JComboBox<Integer> sizeComboBox;
 
-public class WordProgram implements ActionListener {
-    JFrame frame;
+    private JComboBox<String> ColorBox;
+    private JButton boldButton;
+    private JButton italicButton;
+    private JButton underlineButton;
+    private JButton bulletButton;
 
-    JTextArea textArea = new JTextArea(1000,900);
-    private int width = 1280, height = 980;
-    private JScrollPane scrollbar = new JScrollPane(textArea);
-    private int textHeight = 12;
-    private Font currentFont = new Font(Font.SANS_SERIF, 2, textHeight);
+    private JButton LeftAlign;
 
-    JPanel buttonPanel;
+    private JButton RightAlign;
 
-    JPanel buttonPanel2;
+    private JButton MidAlign;
 
+    public WordProgram() {
+        setTitle("Word Processor");
+        setSize(800, 500);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
 
-    JComboBox dropBox1,dropBox2,dropBox3,dropBox4,dropBox5;
+        setLayout(new BorderLayout());
 
-    JButton addBulletButton, removeBulletButton,boldButton, italicizeButton,underlineButton;
+        JToolBar buttons1 = new JToolBar();
+        add(buttons1, BorderLayout.NORTH);
 
-    private JLabel label;
+        GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+        String[] fonts = ge.getAvailableFontFamilyNames();
+        fontComboBox = new JComboBox<>(fonts);
+        fontComboBox.addActionListener(new FontComboBoxListener());
+        buttons1.add(fontComboBox);
 
-    public WordProgram(){
-        frame = new JFrame("Word Document Program");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Integer[] sizes = {8, 10, 12, 14, 16, 18, 20, 24, 28, 32, 36, 40, 48, 56, 64, 72};
+        sizeComboBox = new JComboBox<>(sizes);
+        sizeComboBox.addActionListener(new SizeComboBoxListener());
+        buttons1.add(sizeComboBox);
 
+        // Bold button
+        boldButton = new JButton(new StyledEditorKit.BoldAction());
+        boldButton.setText("Bold");
+        buttons1.add(boldButton);
 
-        buttonPanel = new JPanel();
-        frame.add(buttonPanel, BorderLayout.NORTH);
+        // Italic button
+        italicButton = new JButton(new StyledEditorKit.ItalicAction());
+        italicButton.setText("Italicize");
+        buttons1.add(italicButton);
 
+        // Underline button
+        underlineButton = new JButton(new StyledEditorKit.UnderlineAction());
+        underlineButton.setText("Underline");
+        buttons1.add(underlineButton);
 
-        String l1[]={"10","12","18"};
-        label = new JLabel("Text Size");
-        buttonPanel.add(label);
+        // Bullet button
+        bulletButton = new JButton(new BulletAction());
+        bulletButton.setText("AddBullets");
+        buttons1.add(bulletButton);
 
-        dropBox1 = new JComboBox(l1);
-        buttonPanel.add(dropBox1);
+        // Text pane
+        textPane = new JTextPane();
+        JScrollPane scrollPane = new JScrollPane(textPane);
+        add(scrollPane, BorderLayout.CENTER);
 
-        String l2[]={"Arial","Century Gothic","AmerType","Times New Roman"};
-        label = new JLabel("Text Font");
-        buttonPanel.add(label);
+        JToolBar buttons2 = new JToolBar();
+        add(buttons2, BorderLayout.WEST);
 
-        dropBox2 = new JComboBox(l2);
-        buttonPanel.add(dropBox2);
+        buttons2.setLayout(new GridLayout(20,3));
 
-        String l3[]={"Right","middle","left"};
-        label = new JLabel("Alignment");
-        buttonPanel.add(label);
+        LeftAlign = new JButton(new StyledEditorKit.AlignmentAction("Left Align", StyleConstants.ALIGN_LEFT));
+        LeftAlign.setText("Left");
+        buttons2.add(LeftAlign);
 
-        dropBox3 = new JComboBox(l3);
-        buttonPanel.add(dropBox3);
+        RightAlign = new JButton(new StyledEditorKit.AlignmentAction("Right Align", StyleConstants.ALIGN_RIGHT));
+        RightAlign.setText("Right");
+        buttons2.add(RightAlign);
 
+        MidAlign = new JButton(new StyledEditorKit.AlignmentAction("Right Align", StyleConstants.ALIGN_CENTER));
+        MidAlign.setText("Center");
+        buttons2.add(MidAlign);
 
-        String l4[]={"Black","Red","Yellow","Blue","Green","Orange","Pink","Purple"};
-        label = new JLabel("Color");
-        buttonPanel.add(label);
-
-        dropBox4 = new JComboBox(l4);
-        buttonPanel.add(dropBox4);
-
-        String l5[]={"Black","Red","Yellow","Blue","Green","Orange","Pink","Purple"};
-        label = new JLabel("Highlighter");
-        buttonPanel.add(label);
-
-        dropBox5 = new JComboBox(l5);
-        buttonPanel.add(dropBox5);
-
-        //Buttons:
-        buttonPanel2 = new JPanel();
-        frame.add(buttonPanel2, BorderLayout.EAST);
-
-        addBulletButton = new JButton("Add Bullets");
-        addBulletButton.addActionListener(this);
-        buttonPanel2.add(addBulletButton);
-
-        removeBulletButton = new JButton("Remove Bullets");
-        removeBulletButton.addActionListener(this);
-        buttonPanel2.add(removeBulletButton);
-
-        boldButton = new JButton("Bold");
-        boldButton.addActionListener(this);
-        buttonPanel2.add(boldButton);
-
-        italicizeButton = new JButton("Italicize");
-        italicizeButton.addActionListener(this);
-        buttonPanel2.add(italicizeButton);
-
-        underlineButton = new JButton("Underline");
-        underlineButton.addActionListener(this);
-        buttonPanel2.add(underlineButton);
-
-
+        String[] colors = {"black","green","blue","yellow","pink","red"};
+        ColorBox = new JComboBox<>(colors);
+        ColorBox.addActionListener(new ColorBoxListener());
+        buttons2.add(ColorBox);
 
 
     }
+    private class BulletAction extends StyledEditorKit.StyledTextAction {
 
-    @Override
-    public void actionPerformed(ActionEvent e) {
+        public BulletAction() {
+            super("bullet");
+        }
 
+        public void actionPerformed(ActionEvent e) {
+            JEditorPane editor = (JEditorPane) e.getSource();
+            StyledDocument doc = getStyledDocument(editor);
+
+            int start = editor.getSelectionStart();
+            int end = editor.getSelectionEnd();
+
+            try {
+                doc.insertString(start, "\u2022 ", doc.getStyle(StyleContext.DEFAULT_STYLE));
+            } catch (BadLocationException ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+
+    private class FontComboBoxListener implements ActionListener {
+
+        public void actionPerformed(ActionEvent e) {
+            String selectedFont = (String) fontComboBox.getSelectedItem();
+            MutableAttributeSet attr = new SimpleAttributeSet();
+            StyleConstants.setFontFamily(attr, selectedFont);
+            textPane.setCharacterAttributes(attr, false);
+        }
+    }
+
+    private class ColorBoxListener implements ActionListener {
+
+        public void actionPerformed(ActionEvent e) {
+            String selectedColor = (String) ColorBox.getSelectedItem();
+            MutableAttributeSet colors = new SimpleAttributeSet();
+            if(selectedColor.equals("black")){
+                StyleConstants.setForeground(colors, Color.black);
+            }
+            else if (selectedColor.equals("green")){
+                StyleConstants.setForeground(colors, Color.green);
+            }
+            else if (selectedColor.equals("red")){
+                StyleConstants.setForeground(colors, Color.red);
+            }
+            else if (selectedColor.equals("blue")){
+                StyleConstants.setForeground(colors, Color.blue);
+            }
+            else if (selectedColor.equals("yellow")){
+                StyleConstants.setForeground(colors, Color.yellow);
+            }
+            else if (selectedColor.equals("pink")){
+                StyleConstants.setForeground(colors, Color.pink);
+            }
+
+
+            textPane.setCharacterAttributes(colors, false);
+        }
+    }
+
+    private class SizeComboBoxListener implements ActionListener {
+
+        public void actionPerformed(ActionEvent e) {
+            int selectedSize = (int) sizeComboBox.getSelectedItem();
+            MutableAttributeSet attr = new SimpleAttributeSet();
+            StyleConstants.setFontSize(attr, selectedSize);
+            textPane.setCharacterAttributes(attr, false);
+        }
     }
 
     public static void main(String[] args) {
-        WordProgram x = new WordProgram();
+        WordProgram wordProcessor = new WordProgram();
+        wordProcessor.setVisible(true);
     }
-    class textArea extends JPanel{
-
-        private String mode;
-
-        public textArea(){
-            setBackground(Color.WHITE);
-
-    }
-        public void setMode(String mode) {
-            this.mode = mode;
-        }
-        public void mousePressed(MouseEvent e){
-            if(mode.equals("Add Bullets")){
-
-            }
-            if(mode.equals("Remove Bullets")){
-
-            }
-            if(mode.equals("Bold")){
-
-            }
-            if(mode.equals("Italicize")){
-
-            }
-            if(mode.equals("Underline")){
-
-            }
-        }
-
-
-    }
-
 }
-
